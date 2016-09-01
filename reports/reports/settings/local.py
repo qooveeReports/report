049e01b75a7a6de558a5776f0ae44249759f0eab
nurkan2313 <dzhusupov.nurkan@gmail.com>
2016-09-01 17:45:28 +0600

    remove gitignore

---
+++ reports/reports/settings/local.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'qoovee_report',
+        'USER': 'root',
+        'PASSWORD': '123',
+
+    }
+}
+SECRET_KEY = 'kw=m*wr3v7vjn^93(653^y^0oler2yv3s=y)7s8t)=%ixa@6jt'
